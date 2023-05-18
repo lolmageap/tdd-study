b3a60967efab48b742c8fcd86a9b56384a99d262
lolmageap <ekxk1234@gmail.com>
2023-05-18 17:04:11 +0900

    create baseEntity and domain

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -0,0 +1,29 @@
+package sample.cafekiosk.spring.domain.product;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingType sellingType;
+
+    private String name;
+
+    private int price;
+
+}
