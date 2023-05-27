c9ea8c2e02b0628749250ece6b382370adfad482
cherhy <ekxk1234@naver.com>
2023-05-27 23:06:34 +0900

    Apply @TestFactory and DynamicTest

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
@@ -2,9 +2,43 @@ package sample.cafekiosk.spring.domain.stock;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Collection;
+import java.util.List;
 
 class StockTest {
+
+    @TestFactory
+    @DisplayName("재고 차감 시나리오")
+    Collection<DynamicTest> dynamicTestStockDeduction(){
+
+        Stock stock = Stock.create("001", 1);
+
+        return List.of(
+            DynamicTest.dynamicTest("재고를 주어진 개수만큼 차감 할 수 있다.", () -> {
+                //given
+                int quantity = 1;
+
+                //when
+                stock.deductQuantity(quantity);
+
+                //then
+                Assertions.assertThat(stock.getQuantity()).isZero();
+            }),
+            DynamicTest.dynamicTest("재고보다 많은 수의 수량으로 차감 시도하는 경우 예외가 발생한다.", () -> {
+                //given
+                int quantity = 2;
+
+                //when //then
+                Assertions.assertThatThrownBy(() -> stock.deductQuantity(quantity))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("차감할 재고 수량이 없습니다.");
+            })
+        );
+    }
 
     @Test
     @DisplayName("재고의 수량이 제공된 수량보다 작은지 확인한다.")
